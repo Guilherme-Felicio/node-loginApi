e9aa4684cb6d69738f9221837198cfe962cf786c
Guilherme <guifelicio7@gmail.com>
2023-02-03 13:15:38 -0300

    feat: enusre login route has a httpRequest and a httpRequest body

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -3,7 +3,13 @@ interface HttpRequest {
 }
 
 class LoginRouter {
-  route(httpRequest: HttpRequest) {
+  route(httpRequest: HttpRequest | undefined) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500,
+      };
+    }
+
     const { email, password } = httpRequest.body;
     if (!email || !password) {
       return {
@@ -34,5 +40,18 @@ describe("Login router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(400);
+  });
+
+  test("Should return 500 if no httpRequest is provided", () => {
+    const sut = new LoginRouter();
+    const httpResponse = sut.route(undefined);
+    expect(httpResponse?.statusCode).toBe(500);
+  });
+
+  test("Should return 500 if no httpRequest has no body", () => {
+    const sut = new LoginRouter();
+    const httpRequest: any = {};
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse?.statusCode).toBe(500);
   });
 });
