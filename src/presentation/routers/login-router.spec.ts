2488321a5ac9d6f8303d83bbc0c0720c928e3a40
Guilherme <guifelicio7@gmail.com>
2023-02-03 19:53:04 -0300

    refactor: move sut create to a factory helper method

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,9 +1,13 @@
 import MissingParamError from "../helpers/missing-param-error";
 import LoginRouter from "./login-router";
 
+const makeSut = () => {
+  return new LoginRouter();
+};
+
 describe("Login router", () => {
   test("Should return 400 if no email isProvided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         password: "password",
@@ -15,7 +19,7 @@ describe("Login router", () => {
   });
 
   test("Should return 400 if no password isProvided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest = {
       body: {
         email: "email@email.com",
@@ -27,13 +31,13 @@ describe("Login router", () => {
   });
 
   test("Should return 500 if no httpRequest is provided", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpResponse = sut.route(undefined);
     expect(httpResponse?.statusCode).toBe(500);
   });
 
   test("Should return 500 if no httpRequest has no body", () => {
-    const sut = new LoginRouter();
+    const sut = makeSut();
     const httpRequest: any = {};
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
