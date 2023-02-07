8d5ebd7f90ff94a1d8c5b874a470542b4db727a1
Guilherme <guifelicio7@gmail.com>
2023-02-07 16:45:35 -0300

    feat: move server error to a helper method

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,9 +1,30 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import MissingParamError from "../helpers/missing-param-error";
+import ServerError from "../helpers/server-error";
 import UnauthorizedError from "../helpers/unauthorized-error";
 import LoginRouter, { HttpRequest } from "./login-router";
 
 const makeSut = () => {
+  const authUseCaseSpy = makeAuthUseCaseSpy();
+  // @ts-ignore
+  const sut = new LoginRouter(authUseCaseSpy);
+  return {
+    sut,
+    authUseCaseSpy,
+  };
+};
+
+const makeAuthUseCaseWithError = () => {
+  class AuthUseCaseSpy {
+    auth() {
+      throw new Error();
+    }
+  }
+
+  return new AuthUseCaseSpy();
+};
+
+const makeAuthUseCaseSpy = () => {
   class AuthUseCaseSpy {
     email: string | undefined = "";
     password = "";
@@ -15,14 +36,7 @@ const makeSut = () => {
       return this.accessToken;
     }
   }
-
-  const authUseCaseSpy = new AuthUseCaseSpy();
-  // @ts-ignore
-  const sut = new LoginRouter(authUseCaseSpy);
-  return {
-    sut,
-    authUseCaseSpy,
-  };
+  return new AuthUseCaseSpy();
 };
 
 describe("Login router", () => {
@@ -55,6 +69,7 @@ describe("Login router", () => {
     // @ts-ignore
     const httpResponse = sut.route(undefined);
     expect(httpResponse?.statusCode).toBe(500);
+    expect(httpResponse?.body).toEqual(new ServerError());
   });
 
   test("Should return 500 if no httpRequest has no body", () => {
@@ -62,6 +77,7 @@ describe("Login router", () => {
     const httpRequest: any = {};
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
+    expect(httpResponse?.body).toEqual(new ServerError());
   });
 
   test("Should call auth useCase with correct params", () => {
@@ -118,6 +134,7 @@ describe("Login router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
+    expect(httpResponse?.body).toEqual(new ServerError());
   });
 
   test("Should return 500 if authUseCase has no auth method", () => {
@@ -131,5 +148,21 @@ describe("Login router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
+    expect(httpResponse?.body).toEqual(new ServerError());
+  });
+
+  test("Should return 500 if authUseCase throws error 500", () => {
+    const authUseCaseSpy = makeAuthUseCaseWithError();
+    // @ts-ignore
+    const sut = new LoginRouter(authUseCaseSpy);
+    const httpRequest: HttpRequest = {
+      body: {
+        email: "any_email@email.com",
+        password: "any_password",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse?.statusCode).toBe(500);
+    expect(httpResponse?.body).toEqual(new ServerError());
   });
 });
--- src/presentation/routers/login-router.ts
+++ src/presentation/routers/login-router.ts
@@ -18,26 +18,22 @@ class LoginRouter {
   }
 
   route(httpRequest: HttpRequest): { statusCode: number; body?: any } {
-    if (
-      !httpRequest ||
-      !httpRequest.body ||
-      !this.authUseCase ||
-      !this.authUseCase.auth
-    ) {
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return HttpResponse.badRequest("email");
+      }
+      if (!password) {
+        return HttpResponse.badRequest("password");
+      }
+
+      const accessToken = this.authUseCase.auth(email, password);
+      if (!accessToken) return HttpResponse.UnauthorizedError();
+      return HttpResponse.ok({ accessToken });
+    } catch (err) {
+      console.error(err);
       return HttpResponse.serverError();
     }
-
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return HttpResponse.badRequest("email");
-    }
-    if (!password) {
-      return HttpResponse.badRequest("password");
-    }
-
-    const accessToken = this.authUseCase.auth(email, password);
-    if (!accessToken) return HttpResponse.UnauthorizedError();
-    return HttpResponse.ok({ accessToken });
   }
 }
 
