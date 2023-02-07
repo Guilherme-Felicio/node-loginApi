48b4bfaa43cd544846c97ed9954d7215eea1c703
Guilherme <guifelicio7@gmail.com>
2023-02-07 12:08:15 -0300

    feature: ensure LoginRouter calls auth useCase with correct params

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,13 +1,27 @@
 import MissingParamError from "../helpers/missing-param-error";
-import LoginRouter from "./login-router";
+import LoginRouter, { HttpRequest } from "./login-router";
 
 const makeSut = () => {
-  return new LoginRouter();
+  class AuthUseCaseSpy {
+    email = "";
+    password = "";
+    auth(email: string, password: string) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy();
+  const sut = new LoginRouter(authUseCaseSpy);
+  return {
+    sut,
+    authUseCaseSpy,
+  };
 };
 
 describe("Login router", () => {
   test("Should return 400 if no email isProvided", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: "password",
@@ -19,7 +33,7 @@ describe("Login router", () => {
   });
 
   test("Should return 400 if no password isProvided", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: "email@email.com",
@@ -31,15 +45,28 @@ describe("Login router", () => {
   });
 
   test("Should return 500 if no httpRequest is provided", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpResponse = sut.route(undefined);
     expect(httpResponse?.statusCode).toBe(500);
   });
 
   test("Should return 500 if no httpRequest has no body", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest: any = {};
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
+  });
+
+  test("Should call auth useCase with correct params", () => {
+    const { sut, authUseCaseSpy } = makeSut();
+    const httpRequest: HttpRequest = {
+      body: {
+        email: "email@email.com",
+        password: "password",
+      },
+    };
+    sut.route(httpRequest);
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
   });
 });
--- src/presentation/routers/login-router.ts
+++ src/presentation/routers/login-router.ts
@@ -1,10 +1,22 @@
 import HttpResponse from "../helpers/http-response";
 
-interface HttpRequest {
+export interface HttpRequest {
   body: any;
 }
 
+class AuthUseCase {
+  auth(email: string, password: string) {
+    //
+  }
+}
+
 class LoginRouter {
+  authUseCase: AuthUseCase;
+
+  constructor(authUseCase: AuthUseCase) {
+    this.authUseCase = authUseCase;
+  }
+
   route(httpRequest: HttpRequest | undefined) {
     if (!httpRequest || !httpRequest.body) {
       return HttpResponse.serverError();
@@ -17,6 +29,7 @@ class LoginRouter {
     if (!password) {
       return HttpResponse.badRequest("password");
     }
+    this.authUseCase.auth(email, password);
   }
 }
 
