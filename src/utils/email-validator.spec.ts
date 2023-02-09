dbc68c94e676a60aa9883c4ff03fb0d3e47b64f5
Guilherme <guifelicio7@gmail.com>
2023-02-09 15:43:36 -0300

    refactor: moved sut creation a helper method

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,21 +1,25 @@
 import validator from "../__mocks__/validator";
 
-describe("Email validators tests", () => {
-  class EmailValidator {
-    isValid(email: string) {
-      return validator.isEmail(email);
-    }
+class EmailValidator {
+  isValid(email: string) {
+    return validator.isEmail(email);
   }
+}
 
+const makeSut = () => {
+  return new EmailValidator();
+};
+
+describe("Email validators tests", () => {
   test("should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("valid_email@email.com");
     expect(isEmailValid).toBe(true);
   });
 
   test("should return false if validator returns false", () => {
     validator.isEmailValid = false;
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("@@@@email.com");
     expect(isEmailValid).toBe(false);
   });
