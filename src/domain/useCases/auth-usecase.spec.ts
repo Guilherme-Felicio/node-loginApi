3ae151c116ea3e96187f16a96f300469553bfc57
Guilherme <guifelicio7@gmail.com>
2023-02-10 15:35:39 -0300

    feature: test to ensure authUseCase should call loadUserByEmailRepository with correct email

--- src/domain/useCases/auth-usecase.spec.ts
+++ src/domain/useCases/auth-usecase.spec.ts
@@ -2,10 +2,17 @@ import HttpResponse from "@/presentation/helpers/http-response";
 import { MissingParamError } from "@/utils/errors";
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+
 class AuthUseCase {
+  loadUserByEmailRepository;
+
+  constructor(loadUserByEmailRepository?: any) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository;
+  }
   async auth(email: string, password: string) {
     if (!email) throw new MissingParamError("email");
     if (!password) throw new MissingParamError("password");
+    return await this.loadUserByEmailRepository.load(email);
   }
 }
 
@@ -22,5 +29,19 @@ describe("Auth usecase", () => {
     // @ts-ignore
     const promise = sut.auth("anyEmail@email.com");
     expect(promise).rejects.toThrow();
+  });
+
+  test("should call loadUserByEmailRepository with correct email", async () => {
+    class LoadUserByEmailRepositorySpy {
+      email = "";
+      async load(email: string) {
+        this.email = email;
+      }
+    }
+    const loadUserByEmailRepository = new LoadUserByEmailRepositorySpy();
+
+    const sut = new AuthUseCase(loadUserByEmailRepository);
+    await sut.auth("anyEmail@email.com", "password");
+    expect(loadUserByEmailRepository.email).toBe("anyEmail@email.com");
   });
 });
