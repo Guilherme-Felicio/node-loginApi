f62adf7523e7d4690a1c97247638a054c786f89a
Guilherme <guifelicio7@gmail.com>
2023-02-10 14:46:49 -0300

    feat: new test to ensure authusecase throws missingParamError if no password is provided

--- src/domain/useCases/auth-usecase.spec.ts
+++ src/domain/useCases/auth-usecase.spec.ts
@@ -3,8 +3,9 @@ import { MissingParamError } from "@/utils/errors";
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 class AuthUseCase {
-  async auth(email: string) {
+  async auth(email: string, password: string) {
     if (!email) throw new MissingParamError("email");
+    if (!password) throw new MissingParamError("password");
   }
 }
 
@@ -13,6 +14,13 @@ describe("Auth usecase", () => {
     const sut = new AuthUseCase();
     // @ts-ignore
     const promise = sut.auth();
+    expect(promise).rejects.toThrow();
+  });
+
+  test("should throw if no password is provided", async () => {
+    const sut = new AuthUseCase();
+    // @ts-ignore
+    const promise = sut.auth("anyEmail@email.com");
     expect(promise).rejects.toThrow();
   });
 });
