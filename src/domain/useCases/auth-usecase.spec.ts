ee9c7490b410c4f203d62b9706586b4da2e14eec
Guilherme <guifelicio7@gmail.com>
2023-02-17 20:39:50 -0300

    refactor: moved auth use case wto his own file

--- src/domain/useCases/auth-usecase.spec.ts
+++ src/domain/useCases/auth-usecase.spec.ts
@@ -1,27 +1,7 @@
-import HttpResponse from "@/presentation/helpers/http-response";
 import { InvalidParamError, MissingParamError } from "@/utils/errors";
+import AuthUseCase from "./auth-usecase";
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-
-class AuthUseCase {
-  loadUserByEmailRepository;
-
-  constructor(loadUserByEmailRepository?: any) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository;
-  }
-  async auth(email: string, password: string) {
-    if (!email) throw new MissingParamError("email");
-    if (!password) throw new MissingParamError("password");
-    if (!this.loadUserByEmailRepository)
-      throw new MissingParamError("loadUserByEmailRepository");
-    if (!this.loadUserByEmailRepository?.load)
-      throw new InvalidParamError("loadUserByEmailRepository");
-
-    const user = await this.loadUserByEmailRepository.load(email);
-
-    if (!user) return null;
-  }
-}
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
